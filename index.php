3ced71fdb8b835454e7dd40b746424b786665c04
Luiz Kruger <luizfilipekruger@gmail.com>
2021-03-04 11:59:08 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 <div>Estamos fazendo nosso primeiro deploy na aula</div>
 <div>É do Brasil!!!</div>
 <div>Não desista!!</div>
+<div> A vida pode até te derrubar, mas é você quem escolhe a hora de se levantar.<div>
